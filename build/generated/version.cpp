d3af908e2f5190ecb8ad6962ce5f33c24a610c69
110-kenichi <110.kenichi@gmail.com>
2020-03-23 18:17:02 +0900

    Added files to compile

---
+++ build/generated/version.cpp
@@ -0,0 +1,5 @@
+#define BARE_BUILD_VERSION "211.1" 
+extern const char bare_build_version[]; 
+extern const char build_version[]; 
+const char bare_build_version[] = BARE_BUILD_VERSION; 
+const char build_version[] = BARE_BUILD_VERSION " (mame0211-508-g1440861929-dirty)"; 
